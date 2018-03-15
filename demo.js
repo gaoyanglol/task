83f6ba2475715c0a194d79d0c90f6e9744b7bdd3
gaoyang <gaoyanglol@hotmail.com>
2018-03-16 00:59:22 +0800

    任务5 拔高题

---
+++ demo.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+var fs = require('fs')
+var dirName = process.argv[2]
+fs.stat(dirName, function(err, stat){
+  if (stat && stat.isFile()) {
+    process.exit(0)
+  } else {
+    fs.mkdirSync("./" + dirName)
+    process.chdir("./" + dirName)
+    fs.mkdirSync('css')
+    fs.mkdirSync('js')
+    fs.writeFileSync("./index.html", "<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</h1>")
+    fs.writeFileSync("css/style.css", "h1{color: red;}")
+    fs.writeFileSync("./js/main.js", 'var string = "Hello World"\nalert(string)')
+    process.exit(0)
+  }
+})
+
+
+
